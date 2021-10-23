9346c01107e36f513ee8910d0fd4a662a2a1ff26
Alex Leventis <alex.leventis1@gmail.com>
2021-10-23 15:06:18 -0400

    add port

--- src/app.js
+++ src/app.js
@@ -60,6 +60,8 @@ app.get("*", (req, res) => {
   });
 });
 
-app.listen(3000, () => {
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, () => {
   console.log("Server is up and running");
 });
